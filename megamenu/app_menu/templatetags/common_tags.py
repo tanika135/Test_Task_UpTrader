09c54ca2410937b94089e1925595ed2bd5865d6b
Татьяна <tanika135@mail.ru>
2023-11-24 18:14:50 +0300

    реализовано скрытие и раскрытие пунктов меню

--- megamenu/app_menu/templatetags/common_tags.py
+++ megamenu/app_menu/templatetags/common_tags.py
@@ -4,9 +4,41 @@ from app_menu.models import MenuItem
 register = template.Library()
 
 
-@register.inclusion_tag('app_menu/menu.html')
-def show_top_menu(menu_type=None):
-    menu_items = MenuItem.objects.filter(menu__name=menu_type).select_related('menu')
+class MenuItemNode:
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+        self.active = False
+        self.children = []
+
+
+def find_active_menu_items(path, menu_item):
+    item = MenuItemNode(menu_item.name, menu_item.url)
+
+    if menu_item.url == path:
+        item.active = True
+
+    if menu_item.get_children:
+        for child in menu_item.get_children():
+            child_item = find_active_menu_items(path, child)
+            item.children.append(child_item)
+            if child_item.active:
+                item.active = True
+
+    return item
+
+
+@register.inclusion_tag('app_menu/menu.html', takes_context=True)
+def show_top_menu(context, menu_type=None):
+    menu_items_raw = MenuItem.objects.filter(menu__name=menu_type, level=0).select_related('menu')
+    menu = []
+    for item in menu_items_raw:
+        menu.append(find_active_menu_items(path=context.request.path, menu_item=item))
+
+    print(menu)
+
     return {
-        "menu_items": menu_items,
+        "menu_items": menu,
+        "cur_page": context.request.path,
+        "test": "test"
     }
--- megamenu/megamenu/urls.py
+++ megamenu/megamenu/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from app_menu.views import MenuIndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', MenuIndexView.as_view(), name='index'),
+    re_path(r'.*', MenuIndexView.as_view(), name='index'),
 ]
+
+
